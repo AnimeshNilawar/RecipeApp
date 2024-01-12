246c1e7ef279b4f17a33d849999f8dec9d7244cb
ModdyNerd <148982099+ModdyNerd@users.noreply.github.com>
2024-01-12 18:54:13 +0530

    Recipe App

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,7 +55,7 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("io.coil-kt:coil-compose:2.4.0")
-
+    implementation("androidx.navigation:navigation-compose:2.7.4")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.2")
--- app/src/main/java/com/example/myrecipeapp/RecipeScreen.kt
+++ app/src/main/java/com/example/myrecipeapp/RecipeScreen.kt
@@ -1,6 +1,7 @@
 package com.example.myrecipeapp
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.aspectRatio
@@ -23,7 +24,8 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.rememberAsyncImagePainter
 
 @Composable
-fun RecipeScreen(modifier : Modifier = Modifier){
+fun RecipeScreen(modifier : Modifier = Modifier,
+                 navigateToDetail: (Category) -> Unit){
     val recipeViewModel: MainViewModel = viewModel()
     val viewstate by recipeViewModel.categoriesState
 
@@ -39,28 +41,34 @@ fun RecipeScreen(modifier : Modifier = Modifier){
 
             else -> {
                 // Display Categories
-                CategoryScreen(categories = viewstate.list)
+                CategoryScreen(categories = viewstate.list,
+                    navigateToDetail)
+
             }
         }
     }
 }
 
 @Composable
-fun CategoryScreen(categories : List<Category>){
+fun CategoryScreen(categories : List<Category>,
+                   navigateToDetail: (Category) -> Unit){
     LazyVerticalGrid(GridCells.Fixed(2), modifier = Modifier.fillMaxSize()){
             items(categories){
                 category ->
-                CategoryItem(category = category)
+                CategoryItem(category = category, navigateToDetail )
             }
     }
 }
 
 //How Each Item Looks Like
 @Composable
-fun CategoryItem(category: Category){
+fun CategoryItem(category: Category,
+                 navigateToDetail: (Category) -> Unit){
     Column(modifier = Modifier
         .padding(8.dp)
-        .fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally)
+        .fillMaxSize()
+        .clickable { navigateToDetail(category)},
+        horizontalAlignment = Alignment.CenterHorizontally)
     {
         Image(
             painter = rememberAsyncImagePainter(category.strCategoryThumb),
